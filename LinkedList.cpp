f574e5545c0d4e3613c9a6e15af4e1a8752159a5
rishab_g_coder <85872098+Rishab30Gupta@users.noreply.github.com>
2021-08-14 22:39:38 +0530

    Create LinkedList.cpp

---
+++ LinkedList.cpp
@@ -0,0 +1,27 @@
+//BASIC TEMPLATE
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int data;
+	Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	Node *head = new Node(10);
+	Node *temp1 = new Node (20);
+	Node *temp2 = new Node (30);
+	head->next = temp1;
+	temp1->next = temp2;
+
+	return 0;
+}
