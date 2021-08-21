8337cac1098d21062b575ae21dd7821260c4538c
rishab_g_coder <85872098+Rishab30Gupta@users.noreply.github.com>
2021-08-21 12:58:55 +0530

    Create BinaryTree.cpp

---
+++ BinaryTree.cpp
@@ -0,0 +1,32 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int data;
+	Node *left;
+	Node *right;
+	Node(int val) {
+		data = val;
+		left = right = NULL;
+	}
+};
+
+int main() {                                                           
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	Node *root = new Node(1); //Root node of our tree
+	root->left = new Node(2);
+	root->right = new Node(3);
+	root->left->left = new Node(4);
+	root->left->right = new Node(5);
+	root->right->left = new Node(7);
+	root->right->right = new Node(8);
+	root->left->right->left = new Node(6);
+	root->right->right->left = new Node(9);
+	root->right->right->right = new Node(10);
+	preOrderTraversal(root);
+	return 0;
+}
